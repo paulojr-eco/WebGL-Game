e420b5da4c3950f8d5bedf0a6ebf8d708993cbc8
Paulo Paiva <paulojr.eco@gmail.com>
2023-10-23 19:04:49 -0300

    feat: add ground component

--- src/app/components/Ground.tsx
+++ src/app/components/Ground.tsx
@@ -1,11 +1,20 @@
+import { useGLTF } from '@react-three/drei';
+
 const Ground = () => {
+  const ground = useGLTF('./models/Ground.gltf');
+
   return (
-    <mesh rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -1, 0]} receiveShadow>
-      <planeBufferGeometry args={[100, 100, 1, 1]} />
-      <shadowMaterial transparent opacity={0.75} />
-      <meshStandardMaterial color={'#458745'} />
+    <mesh position={[0, -1, 0]} receiveShadow>
+      <primitive object={ground.scene} />
+      <mesh rotation={[-0.5 * Math.PI, 0, 0]} receiveShadow>
+        <planeBufferGeometry args={[200, 200, 1, 1]} />
+        <shadowMaterial transparent opacity={0.75} />
+        <meshStandardMaterial color={'#458745'} />
+      </mesh>
     </mesh>
   );
 };
+
+useGLTF.preload('./models/Ground.gltf');
 
 export default Ground;
